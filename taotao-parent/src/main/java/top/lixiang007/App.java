8759ff42627949163eca9d71c684ab984224fff2
lixiang <lixiang007top@163.com>
2021-08-24 08:57:48 +0800

    Initial commit

---
+++ taotao-parent/src/main/java/top/lixiang007/App.java
@@ -0,0 +1,13 @@
+package top.lixiang007;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
